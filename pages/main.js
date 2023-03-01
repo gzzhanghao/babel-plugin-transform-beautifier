dfb6d6ee1adae7b7071d102a541aac68176bf330
Jason <jason.daurus@gmail.com>
2023-03-01 17:35:23 +0800

    chore: update pages

---
+++ pages/main.js
@@ -0,0 +1,30 @@
+/* eslint-env browser */
+/* global Babel */
+/* eslint-disable import/no-unresolved, import/extensions */
+
+import prettier from 'https://unpkg.com/prettier@2.8.4/esm/standalone.mjs';
+import parserBabel from 'https://unpkg.com/prettier@2.8.4/esm/parser-babel.mjs';
+
+import beautifierPlugin from './plugin.mjs';
+
+const input = document.getElementById('input');
+const output = document.getElementById('output');
+
+input.addEventListener('input', () => {
+  try {
+    const babelRes = Babel.transform(input.value, {
+      plugins: [
+        beautifierPlugin,
+      ],
+    });
+
+    const prettierRes = prettier.format(babelRes.code, {
+      parser: 'babel',
+      plugins: [parserBabel],
+    });
+
+    output.value = prettierRes;
+  } catch (error) {
+    // noop
+  }
+});
