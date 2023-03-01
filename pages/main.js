8870a50dd8dff0236e4de07afb49d88db1767f13
Jason <jason.daurus@gmail.com>
2023-03-01 19:15:44 +0800

    chore: add default input

--- pages/main.js
+++ pages/main.js
@@ -7,12 +7,8 @@ import parserBabel from 'https://unpkg.com/prettier@2.8.4/esm/parser-babel.mjs';
 
 import beautifierPlugin from './plugin.mjs';
 
-const DEFAULT_INPUT = 'import prettier from"https://unpkg.com/prettier@2.8.4/esm/standalone.mjs";import parserBabel from"https://unpkg.com/prettier@2.8.4/esm/parser-babel.mjs";import beautifierPlugin from"./plugin.mjs";const input=document.getElementById("input"),output=document.getElementById("output");input.addEventListener("input",()=>{try{const t=Babel.transform(input.value,{plugins:[beautifierPlugin]}),e=prettier.format(t.code,{parser:"babel",plugins:[parserBabel],singleQuote:!0});output.value=e}catch(t){}});';
-
 const input = document.getElementById('input');
 const output = document.getElementById('output');
-
-input.value = DEFAULT_INPUT;
 
 input.addEventListener('input', () => {
   try {
