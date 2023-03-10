955ca9a6d272cf2b639d90b17f9d2ec152698f17
Jason <jason.daurus@gmail.com>
2023-03-10 17:38:36 +0800

    chore: update pages

--- pages/main.js
+++ pages/main.js
@@ -10,22 +10,23 @@ import beautifierPlugin from './plugin.mjs';
 const input = document.getElementById('input');
 const output = document.getElementById('output');
 
-input.addEventListener('input', () => {
-  try {
-    const babelRes = Babel.transform(input.value, {
-      plugins: [
-        beautifierPlugin,
-      ],
-    });
+function formatInput() {
+  const babelRes = Babel.transform(input.value, {
+    plugins: [
+      beautifierPlugin,
+    ],
+  });
 
-    const prettierRes = prettier.format(babelRes.code, {
-      parser: 'babel',
-      plugins: [parserBabel],
-      singleQuote: true,
-    });
+  const prettierRes = prettier.format(babelRes.code, {
+    parser: 'babel',
+    plugins: [parserBabel],
+    singleQuote: true,
+  });
 
-    output.value = prettierRes;
-  } catch (error) {
-    // noop
-  }
-});
+  output.value = prettierRes;
+}
+
+input.value = 'var Ju=xe((E0,Uu)=>{var Gu=pt(),Kr=Ru(),$u=Gu.process,Vu=Gu.Deno,Wu=$u&&$u.versions||Vu&&Vu.version,Hu=Wu&&Wu.v8,dt,fr;Hu&&(dt=Hu.split("."),fr=dt[0]>0&&dt[0]<4?1:+(dt[0]+dt[1]));!fr&&Kr&&(dt=Kr.match(/Edge\\/(\\d+)/),(!dt||dt[1]>=74)&&(dt=Kr.match(/Chrome\\/(\\d+)/),dt&&(fr=+dt[1])));Uu.exports=fr});';
+formatInput();
+
+input.addEventListener('input', formatInput);
